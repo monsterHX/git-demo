8ed36d152ef592049121b26a5ffdbfafd42f2825
UserManHX <784753715@qq.com>
2019-12-02 20:27:16 +0800

    信息

--- demo/src/main/java/com/suixingpay/git/GitApplication.java
+++ demo/src/main/java/com/suixingpay/git/GitApplication.java
@@ -20,6 +20,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitApplication {
     public static void main(String[] args) {
+        // 你好 你好
+        // 你好
         SpringApplication.run(GitApplication.class, args);
     }
 }
