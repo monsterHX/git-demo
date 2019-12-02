9c78e7aa65ac90a7671ae07046bea06eeec9929f
UserManHX <784753715@qq.com>
2019-12-02 20:22:20 +0800

    初始化项目

---
+++ demo/src/main/java/com/suixingpay/git/GitApplication.java
@@ -0,0 +1,25 @@
+/**
+ * All rights Reserved, Designed By Suixingpay.
+ *
+ * @author: hexin[he_xin@suixingpay.com]
+ * @date: 2019年12月02日 20时20分
+ * @Copyright 2019 Suixingpay. All rights reserved.
+ * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
+ */
+package com.suixingpay.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hexin[he_xin@suixingpay.com]
+ * @Classname GitApplication
+ * @Description git启动类
+ * @Date 2019-12-02 20:20
+ */
+@SpringBootApplication
+public class GitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GitApplication.class, args);
+    }
+}
